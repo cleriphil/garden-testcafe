32a8528c147b97454b49fe5415fd396b6951a608
cleriphil <cleri.phil@gmail.com>
2020-11-01 13:56:44 -0800

    test add plant to a cell

--- pageobjects/garden-planner-page.js
+++ pageobjects/garden-planner-page.js
@@ -2,12 +2,17 @@ import { Selector } from 'testcafe';
 
 class GardenPlannerPage {
     constructor () {
+        const addRowBtnSelector = Selector('#add-row-btn');
+        const deleteRowBtnSelector = Selector('#delete-row-btn');
+
         this.gardenGrid = Selector('#gridContainer');
-        this.addRowBtn = Selector('#add-row-btn'); 
-        this.addRowToTopOption = Selector('#add-row-btn').sibling('div').child('a').withText('To top');
+        
+        this.addRowBtn = addRowBtnSelector; 
+        this.addRowToTopOption = addRowBtnSelector.sibling('div').child('a').withText('To top');
         this.fourthRowFirstCell = Selector('#r4c0');
-        this.deleteRowBtn = Selector('#delete-row-btn');
-        this.deleteFirstRowOption = Selector('#delete-row-btn').sibling('div').child('a').nth(0);
+        
+        this.deleteRowBtn = deleteRowBtnSelector;
+        this.deleteFirstRowOption = deleteRowBtnSelector.sibling('div').child('a').nth(0);
         this.thirdRowFirstCell = Selector('#r3c0');
     }
 }
--- tests/test.js
+++ tests/test.js
@@ -19,8 +19,14 @@ test('User is able to add a row', async t => {
 
 test('User is able to delete a row', async t => {   
     await t
-    .click(GardenPlannerPage.deleteRowBtn)
-    .click(GardenPlannerPage.deleteFirstRowOption)
-    .expect(GardenPlannerPage.thirdRowFirstCell.exists).notOk();
+        .click(GardenPlannerPage.deleteRowBtn)
+        .click(GardenPlannerPage.deleteFirstRowOption)
+        .expect(GardenPlannerPage.thirdRowFirstCell.exists).notOk();
 });
 
+test('User is able to add a plant to a cell', async t => {   
+    await t
+        .click(Selector('#r0c1'))
+        .click(Selector('.modal-body').child('.list-group').child('button').child().withText('Leeks'))
+        .expect(Selector('#r0c1').child('.plants').child('.plant').child('.leek').exists).ok();
+});
