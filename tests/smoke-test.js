eca3042c8778937af50c729deab93b784d1555a4
cleriphil <cleri.phil@gmail.com>
2020-11-14 12:01:19 -0800

    add validation tests for register form

--- tests/smoke-test.js
+++ tests/smoke-test.js
@@ -3,14 +3,18 @@ import PracticeSitePage from '../pageobjects/practice-site-page.js';
 
 
 fixture `Smoke Tests`
-    .page `https://qa-automation-practice.netlify.app/`;
+    .page `https://qa-automation-practice.netlify.app/`
+    .beforeEach( async t => {
+        await t
+            .click(PracticeSitePage.nav.forms)
+    });
+
+const registerFields = Object.values(PracticeSitePage.registerTxtFields);
 
 test('Create a new user', async t => {  
-    const registerFieldValues = Object.values(PracticeSitePage.registerTxtFields);
     await t
-        .click(PracticeSitePage.nav.forms)
         .click(PracticeSitePage.nav.register);
-    for (const value of registerFieldValues) {
+    for (const value of registerFields) {
         await t
             .typeText(value, 'test');
     }
@@ -20,9 +24,29 @@ test('Create a new user', async t => {
         .expect(PracticeSitePage.message.registerSuccess.exists).ok();
 });
 
+test('Can\'t submit form with no email', async t => {   
+    await t
+        .click(PracticeSitePage.nav.register);
+    for (const value of registerFields) {
+        await t
+            .typeText(value, 'test');
+    }
+    await t
+        .click(PracticeSitePage.registerSubmitBtn)
+        .expect(PracticeSitePage.message.registerSuccess.exists).notOk()
+        .expect(PracticeSitePage.registerEmailField.focused).eql(true);
+});
+
+test('Can\'t submit form with no values', async t => {   
+    await t
+        .click(PracticeSitePage.nav.register)
+        .click(PracticeSitePage.registerSubmitBtn)
+        .expect(PracticeSitePage.message.registerSuccess.exists).notOk()
+        .expect(PracticeSitePage.registerEmailField.focused).eql(true);
+});
+
 test('Login', async t => {   
     await t
-        .click(PracticeSitePage.nav.forms)
         .click(PracticeSitePage.nav.login)
         .typeText(PracticeSitePage.loginEmailField, 'admin@admin.com')
         .typeText(PracticeSitePage.loginPasswordField, 'admin123')
@@ -32,7 +56,6 @@ test('Login', async t => {
 
 test('Login validation', async t => {   
     await t
-        .click(PracticeSitePage.nav.forms)
         .click(PracticeSitePage.nav.login)
         .typeText(PracticeSitePage.loginEmailField, 'admin@admin.com')
         .typeText(PracticeSitePage.loginPasswordField, 'lorem ipsum')
